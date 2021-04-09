1fb14baf7bff49df4c74657365b1565dd8a7181e
= <=>
2021-04-09 14:24:53 +0800

    first commit

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,34 @@
+<div class="header-bottom wow fadeIn">
+    <div class="container">
+       <nav class="main-menu">
+          <div class="navbar-header">
+             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i class="fa fa-bars" aria-hidden="true"></i></button>
+          </div>
+          
+          <div id="navbar" class="navbar-collapse collapse">
+             <ul class="nav navbar-nav">
+                <li><a class="active" href="home.blade.php">Home</a></li>
+                <li><a data-scroll href="about.blade.php">About us</a></li>
+                <li><a data-scroll href="service.blade.php">Services</a></li>
+                <li><a data-scroll href="doctor.blade.php">Doctors</a></li>
+                <li><a data-scroll href="price.blade.php">Price</a></li>
+                <li><a data-scroll href="testimonials.blade.php">Testimonials</a></li>
+                <li><a data-scroll href="getintouch.blade.php">Contact</a></li>
+             </ul>
+          </div>
+       </nav>
+       <div class="serch-bar">
+          <div id="custom-search-input">
+             <div class="input-group col-md-12">
+                <input type="text" class="form-control input-lg" placeholder="Search" />
+                <span class="input-group-btn">
+                <button class="btn btn-info btn-lg" type="button">
+                <i class="fa fa-search" aria-hidden="true"></i>
+                </button>
+                </span>
+             </div>
+          </div>
+       </div>
+    </div>
+ </div>
+</header>
---
+++ resources/views/layout/home.blade.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+   <!-- Basic -->
+   <meta charset="utf-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <!-- Mobile Metas -->
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+   <!-- Site Metas -->
+   <title>Life Care - Responsive HTML5 Multi Page Template</title>
+   <meta name="keywords" content="">
+   <meta name="description" content="">
+   <meta name="author" content="">
+   <!-- Site Icons -->
+   <link rel="shortcut icon" href="assets/images/fevicon.ico.png" type="image/x-icon" />
+   <link rel="apple-touch-icon" href="assets/images/apple-touch-icon.png">
+   <!-- Bootstrap CSS -->
+   <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+   <!-- Site CSS -->
+   <link rel="stylesheet" href="assets/style.css">
+   <!-- Colors CSS -->
+   <link rel="stylesheet" href="assets/css/colors.css">
+   <!-- ALL VERSION CSS -->
+   <link rel="stylesheet" href="assets/css/versions.css">
+   <!-- Responsive CSS -->
+   <link rel="stylesheet" href="assets/css/responsive.css">
+   <!-- Custom CSS -->
+   <link rel="stylesheet" href="assets/css/custom.css">
+   <!-- Modernizer for Portfolio -->
+   <script src="assets/js/modernizer.js"></script>
+   <!-- [if lt IE 9] -->
+   </head>
+   <body class="clinic_version">
+      <!-- LOADER -->
+      <div id="preloader">
+         <img class="preloader" src="assets/images/loaders/heart-loading2.gif" alt="">
+      </div>
+      <!-- END LOADER -->
+      <header>
+         <div class="header-top wow fadeIn">
+            <div class="container">
+               <a class="navbar-brand" href="index.blade.php"><img src="assets/images/logo.png" alt="image"></a>
+               <div class="right-header">
+                  <div class="header-info">
+                     <div class="info-inner">
+                        <span class="icontop"><img src="assets/images/phone-icon.png" alt="#"></span>
+                        <span class="iconcont"><a href="tel:800 123 456">800 123 456</a></span>	
+                     </div>
+                     <div class="info-inner">
+                        <span class="icontop"><i class="fa fa-envelope" aria-hidden="true"></i></span>
+                        <span class="iconcont"><a data-scroll href="mailto:info@yoursite.com">info@Lifecare.com</a></span>	
+                     </div>
+                     <div class="info-inner">
+                        <span class="icontop"><i class="fa fa-clock-o" aria-hidden="true"></i></span>
+                        <span class="iconcont"><a data-scroll href="#">Daily: 7:00am - 8:00pm</a></span>	
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="header-bottom wow fadeIn">
+            <div class="container">
+               <nav class="main-menu">
+                  <div class="navbar-header">
+                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i class="fa fa-bars" aria-hidden="true"></i></button>
+                  </div>
+				  
+                  <div id="navbar" class="navbar-collapse collapse">
+                     <ul class="nav navbar-nav">
+                        <li><a class="active" href="index.html">Home</a></li>
+                        <li><a data-scroll href="#about">About us</a></li>
+                        <li><a data-scroll href="#service">Services</a></li>
+                        <li><a data-scroll href="#doctors">Doctors</a></li>
+                        <li><a data-scroll href="#price">Price</a></li>
+						<li><a data-scroll href="#testimonials">Testimonials</a></li>
+                        <li><a data-scroll href="#getintouch">Contact</a></li>
+                     </ul>
+                  </div>
+               </nav>
+               <div class="serch-bar">
+                  <div id="custom-search-input">
+                     <div class="input-group col-md-12">
+                        <input type="text" class="form-control input-lg" placeholder="Search" />
+                        <span class="input-group-btn">
+                        <button class="btn btn-info btn-lg" type="button">
+                        <i class="fa fa-search" aria-hidden="true"></i>
+                        </button>
+                        </span>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </header>
+      <div id="home" class="parallax first-section wow fadeIn" data-stellar-background-ratio="0.4" style="background-image:url('images/slider-bg.png');">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12 col-sm-12">
+                  <div class="text-contant">
+                     <h2>
+                        <span class="center"><span class="icon"><img src="assets/images/icon-logo.png" alt="#" /></span></span>
+                        <a href="style.css" class="typewrite" data-period="2000" data-type='[ "Welcome to Life Care", "We Care Your Health", "We are Expert!" ]'>
+                        <span class="wrap"></span>
+                        </a>
+                     </h2>
+                  </div>
+               </div>
+            </div>
+            <!-- end row -->
+         </div>
+         <!-- end container -->
+      </div>
